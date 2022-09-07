9bac6f8ac10825691041629676ff264723e0a44d
LIA ONAGA <liarikao2@gmail.com>
2022-09-07 13:06:00 -0300

    Criei os exercícios 008 a 010

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 20
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {  //condição simples, só tem um if (se) não tem o else (senão)
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {  //condição simples, só tem um if (se) não tem o else (senão)
+    console.log('Você é Brasileiro!')
+}
+console.log('Você é Estrangeiro!')
